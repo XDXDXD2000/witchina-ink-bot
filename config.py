e190a96153b91f994f4e89ad007588022b441c88
XDXDXD2000 <horosodanil48@gmail.com>
2026-06-23 19:17:52 +0300

    Fix ADMIN_ID as string with comma

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не задан в .env файле")
 
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+ADMIN_ID = os.getenv("ADMIN_ID", "623268834,566312940")
 
 # ИНФО О БОТЕ
 BOT_AUTHOR = "@akob007_228"
